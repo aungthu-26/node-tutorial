86ba1f8ed7b56bd8bc9628b971b212d6ab928523
Aung Thu <dev104.ovmyanmar@gmail.com>
2025-05-23 10:49:06 +0630

    sample nodejs tutorial server,client,route,redirect

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+//node server,client,routes,redirect test
+
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    //lodash
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('hello')
+    });
+
+    greet();
+    greet();
 
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
     //check url
     let path = './views/';
-
     switch (req.url) {
         case '/':
             path += 'index.html';
@@ -19,14 +30,14 @@ const server = http.createServer((req, res) => {
             path += 'about.html';
             res.statusCode = 200;
             break;
-        case '/about-me':
+        case '/about-us':
             res.statusCode = 301;
             res.setHeader('Location', '/about');
             res.end();
+            break;
         default:
             path += '404.html';
             res.statusCode = 404;
-            break;
     }
 
     //send an html file
@@ -34,10 +45,9 @@ const server = http.createServer((req, res) => {
         if (err) {
             console.log(err);
             res.end();
-        } else {
-            res.write(data);
-            res.end();
         }
+        // res.write(data);
+        res.end(data);
     })
 });
 
